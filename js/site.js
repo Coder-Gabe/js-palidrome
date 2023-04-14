94c40777722a30818e6cf7a61281f5cdc7f16c60
Rexkwondo <87598098+Rexkwondo@users.noreply.github.com>
2023-04-14 15:29:22 -0700

    updated app

--- js/site.js
+++ js/site.js
@@ -1,12 +1,61 @@
-function displayMessage() {
-	let msg = document.getElementById('message').value;
-	//alert(msg);
+function getValues() {
 
-	Swal.fire(
-		{
-			backdrop: false,
-			title: 'App Name',
-			text: msg
-		}
-	);
+
+	let userMessage = document.getElementById('userMessage').value;
+
+	
+	let returnObj = checkPalindrome(userMessage);
+
+	displayMessage(returnObj);
 }
+
+
+function checkPalindrome(userMessage) {
+
+	userMessage = userMessage.toLowerCase();
+
+	let regex = /[^a-z0-9]/gi;
+
+	userMessage = userMessage.replace(regex, "");
+
+
+	let revMessage = [];
+	let returnObj = {};
+
+	for(let index = userMessage.length -1; index >= 0; index = index - 1) {
+		
+		revMessage += userMessage[index];
+	}
+
+	if (revMessage == userMessage ) {
+
+		returnObj.msg = "Good Job, your message is a palindrome"
+	}
+	else {
+		returnObj.msg = "This message is not a palindrome"
+	}
+
+	returnObj.reversed = revMessage;
+
+	return returnObj;
+}
+
+
+function displayMessage(returnObj) {
+
+	document.getElementById('alert').innerHTML = returnObj.msg;
+	document.getElementById('alert').innerHTML = returnObj.msg;
+	document.getElementById('alert').classList.remove('d-none');
+
+
+}
+
+
+
+	// Swal.fire(
+	// 	{
+	// 		backdrop: false,
+	// 		title: 'JS-Rewind',
+	// 		text: msg
+	// 	}
+	// );
